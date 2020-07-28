b7736b17efe91b665343981dc67c699ef19aef66
dit_khy <hyeyeonkim>
2020-07-28 19:02:59 +0900

    [Update] Ch01 renderHtml 함수 추가

--- ch01/after.py
+++ ch01/after.py
@@ -47,9 +47,10 @@ def createStatementData(invoice, plays):
     statementData["totalVolumeCredits"] = totalVolumeCredits(statementData)
     return statementData
 
-def renderPlainText(data):
+def num2usd(cent_value):
+    return f"${cent_value/100:,.2f}"
 
-    num2usd = lambda cent_value: f"${cent_value/100:,.2f}"
+def renderPlainText(data):
 
     ##### main statement code #####
     result = f'청구 내역 (고객 명 : {data["customer"]})\n'
@@ -68,6 +69,22 @@ def main():
     plays = json.load(open('./data/plays.json'))
     return statement(invoice, plays)
 
+
+def htmlStatement(invoice, plays):
+    return renderHtml(createStatementData(invoice, plays))
+
+def renderHtml(data):
+    result = ''
+    result += f'<h1>청구 내역 (고객명: {data["customer"]}</h1>\n'
+    result += '<table>\n'
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>'
+    for perf in data["performances"]:
+        result += f'<tr><td>{perf["play"]["name"]}</td><td>{perf["audience"]}석)</td>'
+        result += f'<td>{num2usd(perf["amoun"])}</td></tr>\n'
+    result += '</table>\n'
+    result += f'<p>총액: <em>${num2usd(data["totalAmount"])}</em></p>\n'
+    result += f'<p>적립 포인트: <em>${data["totalVolumeCredits"]}</em>점</p>\n'
+    return result
 
 if __name__ == '__main__':
     print(main())
