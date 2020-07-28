3c3eee804ff69e3afcc5abbf5565414ac17dd597
dit_khy <hyeyeonkim>
2020-07-28 18:00:20 +0900

    [Update] Ch01 임시 변수를 질의 함수로 변경 및 변수 인라인

--- ch01/after.py
+++ ch01/after.py
@@ -1,35 +1,41 @@
 import json
 
 
-def amountFor(aPerformance, play):
-    result = 0
-    if play["type"] == 'tragedy':
-        result = 40000
-        if aPerformance["audience"] > 30:
-            result += 1000 * (aPerformance["audience"] - 30)
-    elif play["type"] == 'comedy':
-        result = 30000
-        if aPerformance["audience"] > 20:
-            result += 10000 + 500 * (aPerformance["audience"] - 20)
-        result += 300 * aPerformance["audience"]
-    else:
-        raise ValueError(f'알 수 없는 장르: {play["type"]}')
-    return result
+
 
 def statement(invoice, plays):
+
+    def playFor(aPerformance):
+        return plays[aPerformance["playID"]]
+
+    def amountFor(aPerformance):
+        result = 0
+        if playFor(aPerformance)["type"] == 'tragedy':
+            result = 40000
+            if aPerformance["audience"] > 30:
+                result += 1000 * (aPerformance["audience"] - 30)
+        elif playFor(aPerformance)["type"] == 'comedy':
+            result = 30000
+            if aPerformance["audience"] > 20:
+                result += 10000 + 500 * (aPerformance["audience"] - 20)
+            result += 300 * aPerformance["audience"]
+        else:
+            raise ValueError(f'알 수 없는 장르: {playFor(aPerformance)["type"]}')
+        return result
+
     totalAmount = 0
     volumeCredicts = 0
     result = f'청구 내역 (고객 명 : {invoice["customer"]})\n'
     format_ = lambda cent_value: f"${cent_value:,.2f}"
     for perf in invoice["performances"]:
-        play = plays[perf["playID"]]
+        # play = playFor(perf)
 
-        thisAmount = amountFor(perf, play)
+        thisAmount = amountFor(perf)
 
         volumeCredicts += max(perf["audience"] - 30, 0)
-        if play["type"] == "comedy":
+        if playFor(perf)["type"] == "comedy":
             volumeCredicts += perf["audience"] // 5
-        result += f' {play["name"]} : {format_(thisAmount/100)} ({perf["audience"]}석)\n'
+        result += f' {playFor(perf)["name"]} : {format_(thisAmount/100)} ({perf["audience"]}석)\n'
         totalAmount += thisAmount
     result += f'총액: {format_(totalAmount/100)}\n'
     result += f'적립 포인트: {volumeCredicts}점\n'
