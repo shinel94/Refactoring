3f0052f1b686221aa3e0ef301f1a00c6764a5952
dit_khy <hyeyeonkim>
2020-07-28 18:54:19 +0900

    [Update] Ch01 데이터 전처리 과정과 렌더링 과정을 분리

--- ch01/after.py
+++ ch01/after.py
@@ -2,55 +2,64 @@ import json
 
 
 def statement(invoice, plays):
+    return renderPlainText(createStatementData(invoice, plays))
+
+def createStatementData(invoice, plays):
+    statementData = {}
 
     def playFor(aPerformance):
-        return plays[aPerformance["playID"]]
+        return {"play": plays[aPerformance["playID"]]}
 
     def amountFor(aPerformance):
         result = 0
-        if playFor(aPerformance)["type"] == 'tragedy':
+        if aPerformance["play"]["type"] == 'tragedy':
             result = 40000
             if aPerformance["audience"] > 30:
                 result += 1000 * (aPerformance["audience"] - 30)
-        elif playFor(aPerformance)["type"] == 'comedy':
+        elif aPerformance["play"]["type"] == 'comedy':
             result = 30000
             if aPerformance["audience"] > 20:
                 result += 10000 + 500 * (aPerformance["audience"] - 20)
             result += 300 * aPerformance["audience"]
         else:
-            raise ValueError(f'알 수 없는 장르: {playFor(aPerformance)["type"]}')
-        return result
+            raise ValueError(f'알 수 없는 장르: {aPerformance["play"]["type"]}')
+        return {'amount': result}
 
     def volumeCreditsFor(aPerformance):
         result = 0
         result += max(aPerformance["audience"] - 30, 0)
-        if playFor(aPerformance)["type"] == "comedy":
+        if aPerformance["play"]["type"] == "comedy":
             result += aPerformance["audience"] // 5
-        return result
+        return {'volumeCredits': result}
 
-    def totalVolumeCredits():
-        result = 0
-        for perf in invoice["performances"]:
-            result += volumeCreditsFor(perf)
-        return result
+    def totalVolumeCredits(data):
+        return sum([x["volumeCredits"] for x in data["performances"]])
 
-    def totalAmount():
-        result = 0
-        for perf in invoice["performances"]:
-            result += amountFor(perf)
-        return result
+    def totalAmount(data):
+        return sum([x["amount"] for x in data["performances"]])
+
+    statementData["customer"] = invoice["customer"]
+    statementData["performances"] = invoice["performances"]
+    [x.update(playFor(x)) for x in statementData["performances"]] # update에 적절한 코드를 알고 있지 못해서 추후 개선 필요
+    [x.update(amountFor(x)) for x in statementData["performances"]] # update에 적절한 코드를 알고 있지 못해서 추후 개선 필요
+    [x.update(volumeCreditsFor(x)) for x in statementData["performances"]] # update에 적절한 코드를 알고 있지 못해서 추후 개선 필요
+    statementData["totalAmount"] = totalAmount(statementData)
+    statementData["totalVolumeCredits"] = totalVolumeCredits(statementData)
+    return statementData
+
+def renderPlainText(data):
 
     num2usd = lambda cent_value: f"${cent_value/100:,.2f}"
 
     ##### main statement code #####
-    result = f'청구 내역 (고객 명 : {invoice["customer"]})\n'
+    result = f'청구 내역 (고객 명 : {data["customer"]})\n'
 
-    for perf in invoice["performances"]:
-        result += f' {playFor(perf)["name"]} : {num2usd(amountFor(perf))} ({perf["audience"]}석)\n'
+    for perf in data["performances"]:
+        result += f' {perf["play"]["name"]} : {num2usd(perf["amount"])} ({perf["audience"]}석)\n'
 
-    result += f'총액: {num2usd(totalAmount())}\n'
-    result += f'적립 포인트: {totalVolumeCredits()}점\n'
-    
+    result += f'총액: {num2usd(data["totalAmount"])}\n'
+    result += f'적립 포인트: {data["totalVolumeCredits"]}점\n'
+
     return result
 
 
