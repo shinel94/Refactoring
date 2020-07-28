d7f8faae562793881fd7097c55cb7397c166d3a5
dit_khy <hyeyeonkim>
2020-07-28 18:07:19 +0900

    [Update] Ch01 반복 계산문 함수로 추출 및 format_함수명 변경

--- ch01/after.py
+++ ch01/after.py
@@ -23,22 +23,23 @@ def statement(invoice, plays):
             raise ValueError(f'알 수 없는 장르: {playFor(aPerformance)["type"]}')
         return result
 
+    def volumeCreditsFor(aPerformance):
+        volumeCredits = 0
+        volumeCredits += max(aPerformance["audience"] - 30, 0)
+        if playFor(aPerformance)["type"] == "comedy":
+            volumeCredits += aPerformance["audience"] // 5
+        return volumeCredits
+
     totalAmount = 0
-    volumeCredicts = 0
+    volumeCredits = 0
     result = f'청구 내역 (고객 명 : {invoice["customer"]})\n'
-    format_ = lambda cent_value: f"${cent_value:,.2f}"
+    num2usd = lambda cent_value: f"${cent_value/100:,.2f}"
     for perf in invoice["performances"]:
-        # play = playFor(perf)
-
-        thisAmount = amountFor(perf)
-
-        volumeCredicts += max(perf["audience"] - 30, 0)
-        if playFor(perf)["type"] == "comedy":
-            volumeCredicts += perf["audience"] // 5
-        result += f' {playFor(perf)["name"]} : {format_(thisAmount/100)} ({perf["audience"]}석)\n'
-        totalAmount += thisAmount
-    result += f'총액: {format_(totalAmount/100)}\n'
-    result += f'적립 포인트: {volumeCredicts}점\n'
+        volumeCredits += volumeCreditsFor(perf)
+        result += f' {playFor(perf)["name"]} : {num2usd(amountFor(perf))} ({perf["audience"]}석)\n'
+        totalAmount += amountFor(perf)
+    result += f'총액: {num2usd(totalAmount)}\n'
+    result += f'적립 포인트: {volumeCredits}점\n'
     return result
 
 
